c0223d5464d5f21a7ab88b2c11db7d76c742169d
Justin SB <justin@fathomdb.com>
2014-07-01 16:14:39 -0700

    Convert text field to simple button

--- client/client.js
+++ client/client.js
@@ -6,21 +6,13 @@ Template.messages.messages = function () {
 };
 
 Template.input.events = {
-  'keydown input#message' : function (event) {
-    if (event.which == 13) { // 13 is the enter key event
-      if (Meteor.user())
-        var name = Meteor.user().profile.name;
-      else
-        var name = 'Anonymous';
+  'click button#sendyo' : function (event) {
+    if (Meteor.user())
+      var name = Meteor.user().profile.name;
+    else
+      var name = 'Anonymous';
 
-      var message = document.getElementById('message');
-
-      if (message.value != '') {
-        var id = 'message:' + Random.id();
-        Messages.set(id, name);
-        document.getElementById('message').value = '';
-        message.value = '';
-      }
-    }
+    var id = 'message:' + Random.id();
+    Messages.set(id, name);
   }
 }
