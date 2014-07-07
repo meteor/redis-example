a713a4527c3fc1b8afe9e2098f06bccb6e23983f
Slava Kim <slava@meteor.com>
2014-07-07 14:09:13 -0700

    Rename class names, %s/"/'/g

--- client/client.js
+++ client/client.js
@@ -1,19 +1,19 @@
 Session.setDefault('username', null); // start by no name
 
 Template.friendsList.friends = function() {
-  var user = Session.get("username");
-  return user ? Redis.matching("friends::" + user + "::*") : [];
+  var user = Session.get('username');
+  return user ? Redis.matching('friends::' + user + '::*') : [];
 };
 
 Template.friend.events({
   'click li': function () {
-    var user = Session.get("username");
+    var user = Session.get('username');
     if (! user) return;
 
     var ts = (10000000000000 - new Date);
     var toUser = this.value;
     var ttl = 10; // 10 seconds
-    Redis.setex("yo::" + ts + "::" + toUser + "::" + user, ttl, "yo");
+    Redis.setex('yo::' + ts + '::' + toUser + '::' + user, ttl, 'yo');
   }
 });
 
@@ -22,7 +22,7 @@ Template.addFriend.events({
     event.preventDefault();
     var usernameField = template.find('#friend-username')
     var username = usernameField.value.trim().toLowerCase();
-    var newFriend = Redis.get("user::" + username);
+    var newFriend = Redis.get('user::' + username);
 
     usernameField.value = '';
 
@@ -38,37 +38,37 @@ Template.addFriend.events({
 });
 
 function addUserToFriends(newUserId) {
-  var user = Session.get("username");
+  var user = Session.get('username');
   if (! user) return;
 
-  if (! Redis.get("friends::" + user + "::" + newUserId)) {
-    Redis.set("friends::" + user + "::" + newUserId, newUserId);
+  if (! Redis.get('friends::' + user + '::' + newUserId)) {
+    Redis.set('friends::' + user + '::' + newUserId, newUserId);
   }
 }
 
 Template.friend.count = function () {
-  var user = Session.get("username");
+  var user = Session.get('username');
   if (! user) return;
 
   var friend = this.value;
-  return Redis.matching("yo::*::" + friend + "::" + user).count();
+  return Redis.matching('yo::*::' + friend + '::' + user).count();
 };
 
 Template.friend.username = function () {
   var id = this._id;
-  return id ? id.split("::")[2] : "";
+  return id ? id.split('::')[2] : '';
 };
 
 Template.yodors.yodors = function () {
-  var user = Session.get("username");
+  var user = Session.get('username');
   if (! user) return;
 
-  return Redis.matching("yo::*::" + user + "::*");
+  return Redis.matching('yo::*::' + user + '::*');
 };
 
 Template.yodors.username = function () {
   var id = this._id;
-  return id ? id.split("::")[3] : "";
+  return id ? id.split('::')[3] : '';
 };
 
 Template.registerForm.events({
@@ -76,16 +76,16 @@ Template.registerForm.events({
     event.preventDefault();
     var username = template.find('#register-username').value.trim().toLowerCase();
 
-    Redis.set("user::" + username, username);
-    Session.set("username", username);
+    Redis.set('user::' + username, username);
+    Session.set('username', username);
 
     return false;
   }
 });
 
 UI.body.loggedIn = function () {
-  return Session.get("username") ? true : false;
+  return Session.get('username') ? true : false;
 };
 
-document.addEventListener("touchstart", function(){}, true);
+document.addEventListener('touchstart', function(){}, true);
 
